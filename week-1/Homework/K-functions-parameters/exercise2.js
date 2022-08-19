7e6088b885b9d0edcb7094822a1427adc0f555f7
Hafiz Abubaker Nazir <habubakernazir@gmail.com>
2022-08-20 01:15:26 +0200

    almost done

--- week-1/Homework/K-functions-parameters/exercise2.js
+++ week-1/Homework/K-functions-parameters/exercise2.js
@@ -1,5 +1,9 @@
 // Declare your function first
-
+function divide(x,y){
+    return x/y;
+}
 var result = divide(3, 4);
+var secresult = divide(16, 4);
 
 console.log(result);
+console.log(secresult);
--- week-1/Homework/K-functions-parameters/exercise4.js
+++ week-1/Homework/K-functions-parameters/exercise4.js
@@ -1,5 +1,9 @@
 // Declare your function first
+function sum(x,y){
+    return  x+y;
+}
 
 // Call the function and assign to a variable `sum`
 
-console.log(sum);
+var result = sum(3, 4);
+console.log(result);
