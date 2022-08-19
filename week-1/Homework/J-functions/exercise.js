faba628ab1d3fe8445c0282a1a87dfddc3d0da14
Hafiz Abubaker Nazir <habubakernazir@gmail.com>
2022-08-20 00:45:11 +0200

    done

--- week-1/Homework/J-functions/exercise.js
+++ week-1/Homework/J-functions/exercise.js
@@ -1,7 +1,10 @@
 function halve(number) {
   // complete the function here
+  return number*0.5;
 }
 
 var result = halve(12);
-
+var secresult =halve (100)
 console.log(result);
+console.log(secresult);
+
--- week-1/Homework/J-functions/exercise2.js
+++ week-1/Homework/J-functions/exercise2.js
@@ -1,7 +1,12 @@
 function triple(number) {
   // complete function here
+  return number * number * number;
 }
 
 var result = triple(12);
 
+var secresult = triple(2);
+
 console.log(result);
+console.log(secresult);
+
