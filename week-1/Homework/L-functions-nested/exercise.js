5ab7d1d8ce5dd069f08cb97d54ad2bd327408b37
Hafiz Abubaker Nazir <habubakernazir@gmail.com>
2022-08-20 13:02:18 +0200

    done

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -1,5 +1,18 @@
-var mentor1 = "Daniel";
+ var mentor1 = "Daniel";
 var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+function spellUpperCase(){
+    let mentorUpperCase = name.toUpperCase();
+    return mentorUpperCase;
+}
+function greetingMentors(name){
+    console.log('Hello, ${spellUpperCase(name)}')
+}
+greetingMentors(mentor1);
+greetingMentors(mentor2);
+greetingMentors(mentor3);
+greetingMentors(mentor4);
+greetingMentors(mentor5);
+greetingMentors(mentor6);
